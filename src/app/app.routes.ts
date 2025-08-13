5d95ced4d6b128e3c4e3382e24eaa04d7f7e9db7
nati <nsisay49@gmail.com>
2025-08-13 11:08:06 -0600

    done with main pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,34 +8,34 @@ export const routes: Routes = [
       return import('./home/home').then((m) => m.Home);
     },
   },
-  // {
-  //   path: 'candidate',
-  //   loadComponent: () => {
-  //     return import('./candidate/candidate').then((m) => m.Candidate);
-  //   },
-  // },
-  // {
-  //   path: 'company',
-  //   loadComponent: () => {
-  //     return import('./company/company').then((m) => m.Company);
-  //   },
-  // },
-  // {
-  //   path: 'department',
-  //   loadComponent: () => {
-  //     return import('./department/department').then((m) => m.Department);
-  //   },
-  // },
-  // {
-  //   path: 'salary',
-  //   loadComponent: () => {
-  //     return import('./salary/salary').then((m) => m.Salary);
-  //   },
-  // },
-  // {
-  //   path: 'employee',
-  //   loadComponent: () => {
-  //     return import('./employee/employee').then((m) => m.Employee);
-  //   },
-  // },
+  {
+    path: 'recruitment',
+    loadComponent: () => {
+      return import('./recruitment/recruitment').then((m) => m.Recruitment);
+    },
+  },
+  {
+    path: 'company',
+    loadComponent: () => {
+      return import('./employees/employees').then((m) => m.Employees);
+    },
+  },
+  {
+    path: 'request',
+    loadComponent: () => {
+      return import('./requests/requests').then((m) => m.Requests);
+    },
+  },
+  {
+    path: 'employees',
+    loadComponent: () => {
+      return import('./employees/employees').then((m) => m.Employees);
+    },
+  },
+  {
+    path: 'company',
+    loadComponent: () => {
+      return import('./company/company').then((m) => m.Company);
+    },
+  },
 ];
